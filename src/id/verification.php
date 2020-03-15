de90b9a530e64db822037136e7e741e01605e55a
Febri Hidayan <42211742+febrihidayan@users.noreply.github.com>
2020-03-15 22:38:03 +0700

    Create verification.php

---
+++ src/id/verification.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+
+    'verified' => 'Email Anda sudah diverifikasi!',
+    'invalid' => 'Tautan verifikasi tidak sah.',
+    'already_verified' => 'Email sudah diverifikasi.',
+    'user' => 'Kami tidak dapat menemukan pengguna dengan alamat email tersebut.',
+    'sent' => 'Kami telah mengirim email tautan verifikasi Anda!',
+
+];
